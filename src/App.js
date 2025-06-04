51c3a366222287dcb3173699fef7937fe5f775b6
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-06-04 22:03:47 +0100

    start project with clear code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá da Danki Code ;)</h1>
+      <h1>Olá da Danki Code</h1>
     </div>
   );
 }
