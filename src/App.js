1f2aacddf4845e959d678397182a35c4c1a5aa58
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-06-06 16:46:02 +0100

    hooks de estado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./App.css";
-import Header from "./Header";
+// import Header from "./Header";
 
 function App(props) {
+  const [count, setCount] = useState(0);
+  const [nome, setNome] = useState("João");
+  const [numeros, setNumeros] = useState([1, 2, 3, 4, 5]);
+  const [obj, setObj] = useState({ nome: "João", idade: 35 });
+
   return (
-    <div className="App">
-      <Header />
+    <div>
+      {numeros.map(function (val) {
+        return <p>{val}</p>;
+      })}
     </div>
   );
 }
